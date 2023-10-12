be402fc2671b6f51a5f15c19c857192c16c5e2f3
damiangs <cgsdamian@gmail.com>
2023-10-12 16:54:29 -0600

    adding landing page project

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
